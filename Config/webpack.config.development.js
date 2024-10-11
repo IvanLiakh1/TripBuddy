1a43094eac0fbfb85a4999c7230add725aca6c94
IvanLiakh1 <liakh.ivan1@student.uzhnu.edu.ua>
2024-10-11 13:58:21 +0200

    Initial commit

---
+++ Config/webpack.config.development.js
@@ -0,0 +1,6 @@
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.config');
+const config = {
+    mode: 'development',
+};
+module.exports = merge(baseConfig, config);
